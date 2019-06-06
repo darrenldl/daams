05f848e7d1372aaeac6e71278f6cd2a5363313a1
Darren Ldl <darrenldldev@gmail.com>
2019-06-06 17:25:35 +1000

    Added eprint functions

--- src/print_utils.py
+++ src/print_utils.py
@@ -1,12 +1,20 @@
+import sys
+
 indent_size = 4
 
 def gen_padding(indent):
     return indent * indent_size * " "
 
-def printin(indent, *msgs):
-    print((indent * indent_size - 1) * " ", *msgs)
+def printin(indent, *args, **kwargs):
+    print((indent * indent_size - 1) * " ", *args, **kwargs)
 
 def indent_str(indent, msg):
     l = msg.split('\n')
     padding = gen_padding(indent)
     return padding + ("\n" + padding).join(l)
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+def eprintin(indent, *args, **kwargs):
+    eprint((indent * indent_size - 1) * " ", *args, **kwargs)
